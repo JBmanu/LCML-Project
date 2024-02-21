1ab109bcb1c4c5c09c9b70492fa79404945475b3
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-02-21 20:48:53 +0100

    test: create testing for minus

---
+++ res/test/operators/minus/doubleMinus.fool.asm
@@ -0,0 +1,10 @@
+push 0
+push 10
+push -2
+sub
+lfp
+push -2
+add
+lw
+print
+halt
--- src/test/OperatorsTest.java
+++ src/test/OperatorsTest.java
@@ -1,173 +1,29 @@
 package test;
 
-import compiler.*;
-import compiler.exc.IncomplException;
-import compiler.exc.TypeException;
-import compiler.lib.FOOLlib;
-import compiler.lib.Node;
-import compiler.lib.TypeNode;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.jupiter.api.Test;
-import svm.ExecuteVM;
-import svm.SVMLexer;
-import svm.SVMParser;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OperatorsTest {
-    private static final String ROOT_OPERATORS_TEST_FILES = "res/test/operators/";
-
-    // FOOL
-    private FOOLLexer lexer;
-    private FOOLParser parser;
-    private ParseTree st;
-    private final ASTGenerationSTVisitor visitor = new ASTGenerationSTVisitor();
-    private final SymbolTableASTVisitor symtableVisitor = new SymbolTableASTVisitor();
-    private final TypeCheckEASTVisitor typeCheckVisitor = new TypeCheckEASTVisitor();
-
-    // ASM
-    private SVMLexer lexerASM;
-    private SVMParser parserASM;
-
-
-    private CharStream getCharStreams(String fileName) {
-        try {
-            return CharStreams.fromFileName(fileName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void generateST(FOOLParser parser, FOOLLexer lexer) {
-        System.out.println("Generating ST via lexer and parser.");
-        this.st = parser.prog();
-        System.out.println("You had " + lexer.lexicalErrors + " lexical errors and " +
-                parser.getNumberOfSyntaxErrors() + " syntax errors.\n");
-    }
-
-    private Node generateASTAndGetRoot() {
-        System.out.println("Generating AST.");
-        Node ast = this.visitor.visit(this.st);
-        System.out.println();
-        return ast;
-    }
-
-    private void enrichASTSymbolTable(Node ast) {
-        System.out.println("Enriching AST via symbol table.");
-        this.symtableVisitor.visit(ast);
-        System.out.println("You had " + this.symtableVisitor.stErrors + " symbol table errors.\n");
-
-        System.out.println("Visualizing Enriched AST.");
-        new PrintEASTVisitor().visit(ast);
-        System.out.println();
-    }
-
-    private void checkingTypes(Node ast) {
-        System.out.println("Checking Types.");
-        try {
-            TypeNode mainType = this.typeCheckVisitor.visit(ast);
-            System.out.print("Type of main program expression is: ");
-            new PrintEASTVisitor().visit(mainType);
-        } catch (IncomplException e) {
-            System.out.println("Could not determine main program expression type due to errors detected before type checking.");
-        } catch (TypeException e) {
-            System.out.println("Type checking error in main program expression: " + e.text);
-        }
-        System.out.println("You had " + FOOLlib.typeErrors + " type checking errors.\n");
-    }
-
-    private int getFrontEndErrors() {
-        int frontEndErrors = this.lexer.lexicalErrors + this.parser.getNumberOfSyntaxErrors() + this.symtableVisitor.stErrors + FOOLlib.typeErrors;
-        System.out.println("You had a total of " + frontEndErrors + " front-end errors.\n");
-        return frontEndErrors;
-    }
-
-    private void generateCode(Node ast, String fileName) {
-        System.out.println("Generating code.");
-        String code = new CodeGenerationASTVisitor().visit(ast);
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(fileName + ".asm"));
-            out.write(code);
-            out.close();
-            System.out.println();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void assemblingGeneratedCode(String fileName) {
-        System.out.println("Assembling generated code.");
-        this.lexerASM = new SVMLexer(this.getCharStreams(fileName + ".asm"));
-        CommonTokenStream tokensASM = new CommonTokenStream(this.lexerASM);
-        this.parserASM = new SVMParser(tokensASM);
-
-        this.parserASM.assembly();
-    }
-
-    private int getErrorLexerAsm() {
-        System.out.println("You had: " + this.lexerASM.lexicalErrors + " lexical errors and " + this.parserASM.getNumberOfSyntaxErrors() + " syntax errors.\n");
-        return this.lexerASM.lexicalErrors + this.parserASM.getNumberOfSyntaxErrors();
-    }
-
-    private void runningSVM() {
-        System.out.println("Running generated code via Stack Virtual Machine.");
-        ExecuteVM vm = new ExecuteVM(this.parserASM.code);
-        vm.cpu();
-    }
-
-    private void buildASTAndSVMAndCheckErrors(String fileName) {
-        this.lexer = new FOOLLexer(this.getCharStreams(fileName));
-        CommonTokenStream tokens = new CommonTokenStream(this.lexer);
-        this.parser = new FOOLParser(tokens);
-
-        this.generateST(this.parser, this.lexer);
-
-        Node ast = this.generateASTAndGetRoot();
-        this.enrichASTSymbolTable(ast);
-        this.checkingTypes(ast);
-
-        int frontEndErrors = this.getFrontEndErrors();
-        assertEquals(0, frontEndErrors);
-
-        this.generateCode(ast, fileName);
-        this.assemblingGeneratedCode(fileName);
-        assertEquals(0, this.getErrorLexerAsm());
-    }
-
-    private void createFOOLFile(String fileName, String text) {
-        try {
-            File file = new File(fileName);
-
-            if (!file.exists()) file.createNewFile();
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(fw);
-
-            bw.write(text);
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String ROOT_OPERATORS_TEST_FILES = "res/test/operators/minus/";
 
     @Test
     public void testMinusOperator() {
         String fileName = ROOT_OPERATORS_TEST_FILES + "minus.fool";
-        this.createFOOLFile(fileName, "let var x:int = 5 - 3; in print(x);");
 
-        this.buildASTAndSVMAndCheckErrors(fileName);
-        this.runningSVM();
+        ToolsForTest tools = new ToolsForTest();
+        tools.createFOOLFile(fileName, "let var x:int = 5 - 3; in print(x);");
+        tools.buildASTAndSVMAndCheckErrors(fileName, false);
+        tools.runningSVM();
+    }
 
+    // testing 10 --2
+    @Test
+    public void testDoubleMinusOperator() {
+        String fileName = ROOT_OPERATORS_TEST_FILES + "doubleMinus.fool";
 
-
+        ToolsForTest tools = new ToolsForTest();
+        tools.createFOOLFile(fileName, "let var x:int = 10 - -2; in print(x);");
+        tools.buildASTAndSVMAndCheckErrors(fileName, false);
+        tools.runningSVM();
     }
 
 
